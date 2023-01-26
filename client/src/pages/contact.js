951618c0552799f65b3fdc140b44f653943f189e
evancook <evancook711@gmail.com>
2023-01-25 22:13:18 -0500

    added template for contact form

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,7 +1,21 @@
 const Contact = () => (
     <div className='contact'>
       <h1>contact</h1>
+      <div className='email-form-container'>
+        <form className='email-form'>
+          <label for='email'>Email:</label>
+          <input type="email" id='email' name='email'/>
+          <br/>
+          <label for='message'>Write your message below:</label>
+          <textarea rows='4' cols='50' id='message' name='message'></textarea>
+          <br/>
+          <button type='submit' name='submit' id='submit'>Submit</button>
+        </form>
+      </div>
     </div>
+
+    
   );
+
 
 export default Contact
