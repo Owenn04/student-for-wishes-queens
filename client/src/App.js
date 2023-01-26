340fcb365147f9dd907d078d5f680f93e153d945
Huy T <huytruong.eq@gmail.com>
2023-01-25 19:41:45 -0500

    Front End
    - Moved all front-end elements over it
    - Added events page

--- client/src/App.js
+++ client/src/App.js
@@ -12,27 +12,22 @@ import About from './pages/about.js'
 import Volunteer from './pages/volunteer.js'
 import Donate from './pages/donation.js'
 import Contact from './pages/contact'
+import Events from './pages/events';
+import "./App.css"
 
 
 function App() {
   return (
     <div className="App">
       
-      
       <Router>
-        <nav>
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/volunteer'>Volunteer</Link>
-          <Link to='/donate'>Donate</Link>
-          <Link to='/contact'>Contact</Link>
-        </nav>
         <Routes>
           <Route exact path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
           <Route path='/volunteer' element={<Volunteer/>} />
           <Route path='/donate' element={<Donate/>} />
           <Route path='/contact' element={<Contact/>} />
+          <Route path='/events' element={<Events/>} />
         </Routes>
       </Router>
       
