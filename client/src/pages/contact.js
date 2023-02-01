fa34d95e082b8e7e1b7de51225fd6ddfbe326e7b
evancook <evancook711@gmail.com>
2023-02-01 17:13:31 -0500

    added contact form using emailjs

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,21 +1,53 @@
-const Contact = () => (
-    <div className='contact'>
-      <h1>contact</h1>
-      <div className='email-form-container'>
-        <form className='email-form'>
-          <label for='email'>Email:</label>
-          <input type="email" id='email' name='email'/>
-          <br/>
-          <label for='message'>Write your message below:</label>
-          <textarea rows='4' cols='50' id='message' name='message'></textarea>
-          <br/>
-          <button type='submit' name='submit' id='submit'>Submit</button>
-        </form>
-      </div>
-    </div>
+import { send } from 'emailjs-com';
+import {useState} from 'react';
 
+
+const Contact = () => {
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+    });
+
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+        'service_hmg9sdh',
+        'template_nfjugx9',
+        toSend,
+        'qZQcYzrxAdPr9GWWF'
+    )
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+      setToSend({from_name: '', to_name: '', message: '', reply_to: ''});
+    })
+    .catch((err) => {
+        console.log('FAILED...', err);
+    })
+  };
     
-  );
+  const handleChange = (e) => {
+    setToSend({...toSend, [e.target.name]: [e.target.value]});
+  };
+  
+
+
+  return (<div className='contact'>
+    <h1>Contact</h1>
+    <div className='email-form-container'>
+      <form className='email-form' onSubmit={onSubmit}>
+        <input type='text' name='from_name' placeholder='from name' value={toSend.from_name} onChange={handleChange}/>
+        <input type='text' name='to_name' placeholder='to name' value={toSend.to_name} onChange={handleChange}/>
+        <input type='text' name='message' placeholder='Your message' value={toSend.message} onChange={handleChange}/>
+        <input type='text' name='reply_to' placeholder='Your email' value={toSend.reply_to} onChange={handleChange}/>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  </div>);
+
+  };
 
 
 export default Contact
