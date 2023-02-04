359bfe8a0bfb254a978c2e9c6b772905a08ffb41
Huy T <39242676+zuofx@users.noreply.github.com>
2023-02-03 20:54:29 -0500

    Home Page
    - Also updated header

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,7 +2,21 @@ import css from "./css/home.css"
 
 const Home = () => (
     <div className='home'>
-      <h1>home</h1>
+      <div className="home-banner">
+        <a>
+          <h1>WELCOME</h1>
+          <p>TO THE OFFICIAL STUDENTS FOR WISHES QUEEN'S UNIVERSITY WEBSITE</p>
+        </a>
+      </div>
+
+      <div className="home-textbox">
+        <h1>BOX FOR TEXT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod elementum nisi quis eleifend. Diam donec adipiscing tristique risus. Tellus pellentesque eu tincidunt tortor aliquam. Nec feugiat nisl pretium fusce id. Sagittis purus sit amet volutpat consequat. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Etiam erat velit scelerisque in dictum non consectetur a erat.</p>
+      </div>
+
+      {/* Temporary: */}
+      <div className = "spacer"></div>
+      
     </div>
   );
 
