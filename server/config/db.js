78411ee59900c42bdb050c9f7746c7e700e0e2b4
williammacinnis <will@macinnisweb.ca>
2023-02-07 21:45:53 -0500

    Added bulma to client folder and connected db to server

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql')
+const db = mysql.createConnection({
+    host: "qweb.cwfdygwqbs8p.ca-central-1.rds.amazonaws.com",
+    user: "admin",
+    password: "linodeisthebest159",
+    database:"qweb"
+})
+db.connect(function(err) {
+    if (err) throw err;
+    /*
+    db.query("SELECT * FROM tasks", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+     */
+});
+
+module.exports = db;
