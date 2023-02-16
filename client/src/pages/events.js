c849645a0daef14cafaf4dbf8d64997d6aeb3718
Owen Sawler <owensawler4@gmail.com>
2023-02-15 20:13:31 -0500

    added events + mailing code

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -1,7 +1,68 @@
-const Events = () => (
-    <div className='events'>
-      <h1>events</h1>
+import "./css/events.css"
+
+import React, { useState, useEffect } from 'react'
+
+const Events = () => {
+
+  const [events, setEvents] = useState([
+    {
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+      title: "Squirtle Event",
+      date: "novermber 1",
+      location: "queens",
+      description: "yodie gang"
+    },
+    {
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+      title: "Charmander Event",
+      date: "novermber 1",
+      location: "queens",
+      description: "BRURJRHRHRHHRHRH"
+    }
+  ])
+
+  // I just added some preset data for testing
+
+  useEffect(() => {
+    const handleEvents = async () =>{
+      const response = await fetch("url") //no url added
+      const newEvents = await response.json()
+      setEvents(newEvents)
+    }
+    handleEvents()
+  }, [])
+  
+  // Feel Free to change this monstrocity of css. i tried
+
+  if (events != null && events.length > 0){
+    return(
+      <div className='events'>
+        <h1>Events</h1>
+          {events.map((props) => {
+            return(
+            <div className = 'event-item'>
+              <img className = 'event-image' src = {props.image} alt = {props.title}/>
+              <span className = 'event-header'>
+                <h3 className = 'event-title'>{props.title}</h3>
+                <p className = 'event-date'>Date: {props.date} </p>
+              </span>
+              <span className = 'event-info'>
+                <p className = 'event-location'>Location: {props.location} </p>
+                <p className = 'event-description'>{props.description}</p>
+                <button className = 'event-join-button'>Join!</button>
+              </span> 
+            </div>
+            )
+          })}
+      </div>
+    )
+  } else {
+    <div className = 'events'>
+      <h1>Events</h1>
+        <h3>Events Coming Soon</h3>
     </div>
-  );
+  }
+  
+  };
 
 export default Events
--- client/src/pages/mailing.js
+++ client/src/pages/mailing.js
@@ -1,7 +1,58 @@
-const Mailing = () => (
-    <div className='mailing'>
-      <h1>mailing</h1>
-    </div>
-  );
+import {useState} from 'react';
 
+
+const Mailing = () => {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+    console.log("works") 
+  }
+
+  const handleSubscribe = async (e) => {
+    await fetch("http://localhost:3001/mailing", { //no url yet
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+        .then((res) => res.json())
+        .then(setName(""))
+        .then(setEmail(""))
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    handleSubscribe()
+  }
+
+
+
+  return (
+  <div className='/mailing'>
+      <form onSubmit = {handleSubmit}>
+        <input
+        placeholder = 'Name'
+        type = 'text'
+        value = {name}
+        onChange = {handleNameChange}
+        />
+        <input
+        placeholder = 'email'
+        type = 'text'
+        value = {email}
+        onChange = {handleEmailChange}
+        />
+        <button type = 'submit'>Sign Up!</button>
+      </form>
+  </div>
+  )
+}
 export default Mailing
