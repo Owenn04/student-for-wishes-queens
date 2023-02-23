cbc863ae2eb7539ed41fa51976789932b20221ff
Owen Sawler <owensawler4@gmail.com>
2023-02-23 00:40:09 -0500

    events doesnt work with postman. also it seems like nodemon isnt working

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -4,38 +4,15 @@ import React, { useState, useEffect } from 'react'
 
 const Events = () => {
 
-  const [events, setEvents] = useState([
-    {
-      image: "https://images.unsplash.com/photo-1676806995068-fe6c530423dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      title: "Placeholder Event",
-      date: "March 13th 2023",
-      location: "Jefferey 128",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod elementum nisi quis eleifend. Diam donec adipiscing tristique risus. Tellus pellentesque eu tincidunt tortor aliquam. Nec feugiat nisl pretium fusce id. Sagittis purus sit amet volutpat consequat. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Etiam erat velit scelerisque in dictum non consectetur a erat.",
-      link: "http://www.zuofx.xyz"
-    },
-    {
-      image: "https://images.unsplash.com/photo-1538688423619-a81d3f23454b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      title: "Placeholder Event",
-      date: "April 24th 2023",
-      location: "Stirling Auditorium",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod elementum nisi quis eleifend. Diam donec adipiscing tristique risus. Tellus pellentesque eu tincidunt tortor aliquam. Nec feugiat nisl pretium fusce id. Sagittis purus sit amet volutpat consequat. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Etiam erat velit scelerisque in dictum non consectetur a erat.",
-      link: "http://www.zuofx.xyz"
-    },
-    {
-      image: "https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      title: "Placeholder Event",
-      date: "April 17th, 2023",
-      location: "Victoria Hall",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod elementum nisi quis eleifend. Diam donec adipiscing tristique risus. Tellus pellentesque eu tincidunt tortor aliquam. Nec feugiat nisl pretium fusce id. Sagittis purus sit amet volutpat consequat. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Etiam erat velit scelerisque in dictum non consectetur a erat.",
-      link: "http://www.zuofx.xyz"
-    }
-  ])
+  const [events, setEvents] = useState([])
 
   useEffect(() => {
+    console.log("data fetched")
     const handleEvents = async () =>{
-      const response = await fetch("url") //no url added
+      const response = await fetch("http://localhost:3002/api/events/get")
       const newEvents = await response.json()
-      setEvents(newEvents)
+      await setEvents(newEvents)
+      console.log(newEvents)
     }
     handleEvents()
   }, [])
@@ -48,18 +25,18 @@ const Events = () => {
             <h1>EVENTS</h1>
           </a>
         </div>
-          {events.map((props) => {
+          {events.map((props, i) => {
             return(
-            <div className = 'event-item'>
-              <img className = 'event-image' src = {props.image} alt = {props.title}/>
+            <div key = {props.Id} className = 'event-item'>
+              <img className = 'event-image' src = {props.Image} alt = {props.Title}/>
               <div className ="event-textitems">
                 <div className="event-toptext">
-                  <h1 className ="event-title">{props.title}</h1>
-                  <p className = 'event-date'>Date: {props.date} </p>
-                  <p className = 'event-location'>Location: {props.location} </p>
+                  <h1 className ="event-title">{props.Title}</h1>
+                  <p className = 'event-date'>Date: {props.Date} </p>
+                  <p className = 'event-location'>Location: {props.Location} </p>
                 </div>
-                <p className = 'event-description'>{props.description}</p>
-                <a href={props.link} target="_blank">
+                <p className = 'event-description'>{props.Description}</p>
+                <a href="" target="_blank">
                   <button className="event-join">JOIN!</button>
                 </a>
               </div>
